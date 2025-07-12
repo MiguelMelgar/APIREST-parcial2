b36b7a0c8f929ff7a8c616af6af5c02984e80ba1
Yanela <karlapolonio02@gmail.com>
2025-07-11 19:52:38 -0500

    Configuración del proyecto y archivos de inicio para depuración

--- ApiCursos/Program.cs
+++ ApiCursos/Program.cs
@@ -10,39 +10,42 @@ namespace ApiCursos
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Configuración de la base de datos
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            // Configuración de los controladores y JSON
             builder.Services.AddControllers().AddJsonOptions(x =>
                 x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 
+            // Configuración de Swagger
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            builder.Services.AddCors();
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
-            //Habilitar CORS
+            // Configuración de CORS
             builder.Services.AddCors(options =>
             {
-                options.AddPolicy("Permitir todo",
+                options.AddPolicy("PermitirTodo",
                     policy => policy.AllowAnyOrigin()
                                     .AllowAnyHeader()
                                     .AllowAnyMethod());
             });
+
+            var app = builder.Build();
+
+            // Configuración del entorno de desarrollo
+     //       if (app.Environment.IsDevelopment())
+         //   {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+          //  }
+
+            // Configuración de los middlewares
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            // Uso de la política CORS configurada
+            app.UseCors("PermitirTodo");
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
